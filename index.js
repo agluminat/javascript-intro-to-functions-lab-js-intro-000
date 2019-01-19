532d9769d6c066f708eb1313cbbadf17ee9a15c2
Maxi Learner <sagmeister@gmail.com>
2019-01-19 17:20:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   return "I can't hear you!"
-    if sayHiToGrandma.toLowerCase() === lowercase
+    if string.toLowerCase() === lowercase
 }
