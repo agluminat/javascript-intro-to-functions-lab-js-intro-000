28362120f52f98f3c07ac768119d4fd004544dc0
Maxi Learner <sagmeister@gmail.com>
2019-01-19 17:46:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,8 +20,9 @@ function sayHiToGrandma(string) {
   }
 }
 
-function sayHiToGrandma(string) {
+/*function sayHiToGrandma(string) {
   if (string.toUpperCase() === string) {
     return "YES INDEED!"
   }
 }
+8/
